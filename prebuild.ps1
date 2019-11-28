a650ebfe80f7874a45bf8b762370deb54855b2db
Fabian Nawratil <fabian@nawratil.me>
2019-11-28 13:57:11 +0100

    Fix github actions ffmpeg env

--- prebuild.ps1
+++ prebuild.ps1
@@ -4,4 +4,4 @@ $progressPreference = 'silentlyContinue'
 Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip -OutFile ffmpeg.zip
 Expand-Archive ffmpeg.zip -DestinationPath .
 $location = Resolve-Path -Path "ffmpeg-latest-win64-static\bin\ffmpeg.exe"
-[System.Environment]::SetEnvironmentVariable('FFMPEG', $location, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('FFMPEG', $location, [System.EnvironmentVariableTarget]::Process)
