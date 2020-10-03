0f49f59dd77a3794fc9d4d9dbcf1817831716bd2
jvbsl <jvsbl@gmx.de>
2020-10-03 17:08:30 +0200

    Download ffmpeg from new source

--- prebuild.ps1
+++ prebuild.ps1
@@ -1,7 +1,21 @@
+function DownloadWithRedirect([string]$url, [string]$outputFile) {
+    $request = Invoke-WebRequest -Uri $url -MaximumRedirection 1 -SkipHttpErrorCheck -Method "OPTIONS"
+    if($request.StatusCode -ge 300 -and $request.StatusCode -lt 400)
+    {
+        DownloadWithRedirect -url $request.Headers.Location -outputFile $outputFile
+    }
+    elseif($request.StatusCode -eq 200)
+    {
+        Invoke-WebRequest -Uri $url -OutFile $outputFile
+    }
+}
+
 New-Item -ItemType Directory -Force -Path tools
 cd tools
 $progressPreference = 'silentlyContinue'
-Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip -OutFile ffmpeg.zip
+Write-Output "Downloading newest ffmpeg from https://www.gyan.dev/ffmpeg/builds/"
+DownloadWithRedirect -url "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip" -outputFile "ffmpeg.zip"
+Write-Output "Expanding ffmpeg archive"
 Expand-Archive ffmpeg.zip -DestinationPath .
-$location = Resolve-Path -Path "ffmpeg-latest-win64-static\bin\ffmpeg.exe"
+$location = Resolve-Path -Path "*\bin\ffmpeg.exe"
 [System.Environment]::SetEnvironmentVariable('FFMPEG', $location, [System.EnvironmentVariableTarget]::Process)
